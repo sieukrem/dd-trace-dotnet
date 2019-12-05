7e8c6330b94969bd64a21ca3f440a8f212e22fb8
colin-higgins <developerspike@gmail.com>
2019-12-05 10:01:17 -0500

    Azure App Services Package with tooling enhancements

---
+++ deploy/AzureAppServices/content/install.cmd
@@ -0,0 +1,2 @@
+mkdir %WEBROOT_PATH%\app_data\jobs\continuous\datadog-trace-agent
+copy /y trace-agent.exe %WEBROOT_PATH%\app_data\jobs\continuous\datadog-trace-agent
---
+++ tools/PrepareRelease/Program.cs
@@ -0,0 +1,41 @@
+using System.Linq;
+using Castle.Core.Internal;
+
+namespace PrepareRelease
+{
+    public class Program
+    {
+        public const string Versions = "versions";
+        public const string Integrations = "integrations";
+        public const string Msi = "msi";
+        public const string AzureAppServices = "aas";
+
+        public static void Main(string[] args)
+        {
+            if (JobShouldRun(Versions, args))
+            {
+                SetAllVersions.Run();
+            }
+
+            if (JobShouldRun(Integrations, args))
+            {
+                GenerateIntegrationDefinitions.Run();
+            }
+
+            if (JobShouldRun(Msi, args))
+            {
+                SyncMsiContent.Run();
+            }
+
+            if (JobShouldRun(AzureAppServices, args))
+            {
+                SyncAzureAppServicesPackage.Run();
+            }
+        }
+
+        private static bool JobShouldRun(string jobName, string[] args)
+        {
+            return args.IsNullOrEmpty() || args.Any(a => a.ToLower().Equals(jobName));
+        }
+    }
+}
