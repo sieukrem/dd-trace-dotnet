573c217b0167bff063bc617b6456b8326d93f19e
colin-higgins <developerspike@gmail.com>
2019-12-23 12:11:51 -0500

    Start things from the profiler

---
+++ samples/Samples.AspNetCoreMvc2/run-trace-agent.bat
@@ -0,0 +1,3 @@
+REM Check if the trace agent port is available, start an instance if it is
+echo The trace agent port is available, starting the trace agent
+START "C:\Github\ReRommer\ReRommer.FileSniffer.exe"
--- tools/Datadog.Core.Tools/TracerVersion.cs
+++ tools/Datadog.Core.Tools/TracerVersion.cs
@@ -18,7 +18,7 @@ namespace Datadog.Core.Tools
         /// <summary>
         /// The patch portion of the current version.
         /// </summary>
-        public const int Patch = 19;
+        public const int Patch = 28;
 
         /// <summary>
         /// Whether the current release is a pre-release
