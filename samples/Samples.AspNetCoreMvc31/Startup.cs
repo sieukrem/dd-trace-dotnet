0c4b97510f2f381cff541aca8cceb1e1846adb96
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2020-01-15 11:48:03 -0500

    DELETE ME

--- samples/Samples.AspNetCoreMvc31/Startup.cs
+++ samples/Samples.AspNetCoreMvc31/Startup.cs
@@ -19,6 +19,7 @@ namespace Samples.AspNetCoreMvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDatadogTracing();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
